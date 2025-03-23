bb657b642006de81260a27d4480ced8fe3ec0567
kiram <kiramrahim@gmail.com>
2025-03-23 20:46:53 +0000

    feat: adding base faker implementation

---
+++ interfaces/faker.go
@@ -0,0 +1,16 @@
+package interfaces
+
+import(
+	"github.com/kream404/scratch/models"
+)
+var format string;
+var datatype models.Type;
+
+type Faker[T any] interface {
+	Generate() (T, error);
+		GetType() models.Type;
+}
+
+func getType() models.Type{
+	return datatype;
+}
