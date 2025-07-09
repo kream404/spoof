18b5023825af32458b567100148bae0480905290
kiram <kiramrahim@gmail.com>
2025-07-09 21:19:51 +0100

    first increment version

--- cmd/version.go
+++ cmd/version.go
@@ -2,15 +2,16 @@ package cmd
 
 import (
 	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Give cli version",
-	Long: `All software has versions, this is spoof's`,
+	Long:  `All software has versions, this is spoof's`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("0.0.1")
+		fmt.Println("0.0.2")
 	},
 }
 
