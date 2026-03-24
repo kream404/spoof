1c7f7e68e70b5d14457329648be5c252f08d3916
kream404 <kiramrahim@gmail.com>
2026-03-24 13:07:07 +0000

    removing comment

--- services/csv/csv_writer.go
+++ services/csv/csv_writer.go
@@ -21,7 +21,7 @@ import (
 
 	"github.com/kream404/spoof/models"
 	"github.com/kream404/spoof/services/database"
-	"github.com/kream404/spoof/services/evaluator" // ✅ new
+	"github.com/kream404/spoof/services/evaluator"
 	json_parser "github.com/kream404/spoof/services/json"
 	log "github.com/kream404/spoof/services/logger"
 	s3c "github.com/kream404/spoof/services/s3"
